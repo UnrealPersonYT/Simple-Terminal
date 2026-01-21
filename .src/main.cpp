cf1938004c5a80c3b2e8dca8c154b22dbd845c3f
UnrealPersonYT <Malu.Leite@outlook.com>
2026-01-21 06:06:58 -0500

    Now prints version on startup

--- .src/main.cpp
+++ .src/main.cpp
@@ -4,6 +4,8 @@
 using namespace SimpleTerminal;
 
 i32 main(){
+    // Print terminal version on startup
+    Terminal::digest("version");
     while(true){
         std::string inputLine;
         std::cout << "Simple-Terminal: ";
